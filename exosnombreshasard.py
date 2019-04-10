c253d601c0b46930b90ecd20060fb47f877e61c2
AimeeCarrera <49400035+AimeeCarrera@users.noreply.github.com>
2019-04-10 17:25:59 +0200

    Add files via upload

---
+++ exosnombreshasard.py
@@ -0,0 +1,5 @@
+x=int(input("veuillez entrer en nombre entier"))
+if x <= 9 :
+      print("le nombre affiché est dans l'intervalle")
+else:
+      print("le nombre affiché n'est pas dans l'intervalle")
