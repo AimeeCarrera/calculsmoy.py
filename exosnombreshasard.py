89f7ef18e9bbdad1bb1702c1a749a9becfc03995
AimeeCarrera <49400035+AimeeCarrera@users.noreply.github.com>
2019-04-11 11:25:04 +0200

    Delete exosnombreshasard.py

--- exosnombreshasard.py
+++
@@ -1,5 +0,0 @@
-x=int(input("veuillez entrer en nombre entier"))
-if x <= 9 :
-      print("le nombre affiché est dans l'intervalle")
-else:
-      print("le nombre affiché n'est pas dans l'intervalle")
